d5b76d61e17aca31da781ef9ceebd77a66768ec1
ahmedgamer4 <aa10@pme.suezuni.edu.eg>
2022-08-31 15:25:06 +0200

    Finished

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ function Book(title, author, pages, isread) {
     this.isread = isread;
 }
 
+Book.prototype.toggleRead = function() {
+    this.isread = this.isread ? false : true
+}
+
 function Library() {
     this.books = [];
 }
@@ -38,7 +42,7 @@ Library.prototype.addBook = function(newBook) {
 // const addBookToLibrary = book => {
 //     Library.push(book)
 // }
-let library = new Library()
+let library = new Library();
 
 const getBook = () => {
     const title = document.getElementById('title').value;
@@ -56,33 +60,53 @@ const addBook = e => {
     if (!library.inLibrary(newBook)) {
         library.addBook(newBook);
         pop.classList.remove('active');
+        // const reset = inputs.forEach(e => {
+        //     e.value = '';
+        // });
+    }
+    else {
+        pop.classList.remove('active');
         const reset = inputs.forEach(e => {
-            e.value = '';
+           e.value = '';
         });
     }
-    display(newBook);
+    display();
 }
 
-const display = (book) => {
-    let card = document.createElement('div');
-    let title = document.createElement('p');
-    let author = document.createElement('p');
-    let pages = document.createElement('p');
-    let buttonGroup = document.createElement('div');
-    let read = document.createElement('button');
-    let remove = document.createElement('button');
+const display = () => {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => bottomMain.removeChild(card));
 
-    read.onclick = toggleRead(book);
+    for (let i = 0; i < library.books.length; i++) {
+        // bottomMain.removeChild(bottomMain.firstchild)
+        createCard(library.books[i]);
+    }
+}
+
+const createCard = (item) => {
+    // const bookIndex = library.books.length - 1;
+    // const newestBook = library.books[bookIndex];
+    
+
+    const card = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const buttonGroup = document.createElement('div');
+    const read = document.createElement('button');
+    const remove = document.createElement('button');
+
+    read.onclick = item.isread = this.isread ? false : true;
 
     card.classList.add('card');
     buttonGroup.classList.add('button-group');
     remove.classList.add('btn');
     remove.textContent = 'remove';
-    title.textContent = `"${book.title}"`;
-    author.textContent = book.author;
-    pages.textContent = book.pages;
+    title.textContent = `"${item.title}"`;
+    author.textContent = item.author;
+    pages.textContent = item.pages;
 
-    if (book.isread === true) {
+    if (item.isread === true) {
         read.textContent = "read";
         read.className = 'btn-green';
     }
@@ -91,7 +115,25 @@ const display = (book) => {
         read.className = 'btn-red';
     }
 
+    remove.onclick = e => {
+        library.books.splice(library.books.indexOf(item), 1);
+        // display();
+        card.remove();
+    }
 
+    read.onclick = () => {
+        item.isread = ! item.isread;
+        if (item.isread === true) {
+            read.textContent = "read";
+            read.className = 'btn-green';
+        }
+        else {
+            read.textContent = 'Not read';
+            read.className = 'btn-red';
+        }
+    }
+    
+    // card.setAttribute('data', bookIndex);
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
@@ -110,6 +152,10 @@ const toggleRead = book => {
     }
 }
 
+// const remove = function() {
+    
+// }
+
 
 add.addEventListener('click', e => {
     pop.classList.toggle("active");
@@ -117,6 +163,7 @@ add.addEventListener('click', e => {
 });
 
 form.onsubmit = addBook;
+
 
 // submit.onclick =  pop.classList.remove('active');
 // let book = getBook()
